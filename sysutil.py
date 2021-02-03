411d3738da4e0d4617a7ca4cc8744776c4578f40
Xiukun Hu <xihu@aurora.tech>
2021-02-03 13:34:26 -0800

    fix

--- sysutil.py
+++ sysutil.py
@@ -2,7 +2,7 @@ from color import warn, okay
 import os
 import shutil
 import shlex
-from subprocess import run, check_call
+from subprocess import run, check_call, check_output
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 HOME = os.getenv('HOME')
@@ -16,15 +16,13 @@ def run_cmd(commands: str):
     check_call(shlex.split(commands))
 
 
-def link(path_from_this: str, dst_path: str, backup: str):
-    if os.path.exists(dst_path):
-        warn(f'{dst_path} already exists. Moving it to {backup}')
-        shutil.move(dst_path, backup)
-    src_path = os.path.join(SCRIPT_DIR, path_from_this)
-    run_cmd(f'ln -s {src_path} {dst_path}')
-    okay(f"Created link:\n\t{src_path} ->\n\t\t{dst_path}")
+def link(src: str, dst_path: str, backup_suffix: str = '.bk'):
+    run_cmd(f'ln -s --suffix={backup_suffix} {src} {dst_path}')
+    okay(f"Created link:")
+    okay(check_output(
+        f'ls -l {os.path.join(dst_path, os.path.basename(src))}'.split()))
 
 
-def link_to_home(path_from_this: str, backup_suffix: str='.bk'):
-    link(path_from_this, os.path.join(HOME, path_from_this),
-         os.path.join(HOME, path_from_this + backup_suffix))
+def link_to_home(path_from_this: str, backup_suffix: str = '.bk'):
+    link(path_from_this, os.path.join(
+        HOME, os.path.dirname(path_from_this)), backup_suffix)
