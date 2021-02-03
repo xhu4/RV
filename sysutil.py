125a32770e72dac0907efb6fdeafa7cece01519d
Xiukun Hu <xihu@aurora.tech>
2021-02-03 15:15:46 -0800

    fix link

--- sysutil.py
+++ sysutil.py
@@ -17,15 +17,19 @@ def run_cmd(commands: str):
     check_call(shlex.split(commands))
 
 
-def add_suffix(path:str, suffix:str):
-    return os.path.join(os.path.dirname(path), os.path.basename(path)+suffix)
+def add_suffix(path: str, suffix: str):
+    return os.path.join(os.path.dirname(path), os.path.basename(path) + suffix)
 
 
 def link(src: str, dst_path: str, backup_suffix: str = '.bk'):
-    run_cmd(f'ln -bs --suffix={backup_suffix} {os.path.join(SCRIPT_DIR, src)} {dst_path}')
+    if has(dst_path):
+        shutil.move(dst_path, add_suffix(dst_path, backup_suffix))
+    run_cmd(f'ln -bs --suffix={backup_suffix} -T {os.path.join(SCRIPT_DIR, src)} {dst_path}')
     okay(f"Created link:")
-    okay(check_output(f'ls -l {os.path.join(dst_path, os.path.basename(src))}'.split()))
+    okay("\t", check_output(f'ls -ld1 {dst_path}'.split()).decode('utf-8'))
 
 
 def link_to_home(path_from_this: str, backup_suffix: str = '.bk'):
-    link(path_from_this, os.path.join(HOME, os.path.dirname(path_from_this), os.path.basename(path_from_this)), backup_suffix)
+    link(path_from_this,
+         os.path.join(HOME, os.path.dirname(path_from_this), os.path.basename(path_from_this)),
+         backup_suffix)
