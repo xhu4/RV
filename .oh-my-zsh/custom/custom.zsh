2f79d9f82a51dd1dd0dabfe83c5455e6d1a9d94e
Xiukun Hu <xiukun.hu@outlook.com>
2021-02-05 13:50:58 -0800

    var AV

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -1,6 +1,6 @@
 alias ec='nvim ~/.oh-my-zsh/custom/custom.zsh'
 alias ag='alias | grep'
-alias av='nvim ~/codes/av'
+alias av='nvim $AV'
 alias sc='source ~/.zshrc'
 alias nvrc='nvim ~/.config/nvim/init.vim'
 alias gb='git br'
@@ -8,18 +8,27 @@ alias gbd='git br -d'
 alias buildserver='gcloud compute --project "aurora-localization-dev" ssh --zone "us-east1-d" --ssh-flag="-ServeAliveInterval=30" xihu-mapbuild-0 -- -L 8081:localhost:80'
 
 findtile() {
-    radius=${3:-100}
-    ~/codes/av/bazel-bin/atlas/tools/find_tiles_near_latlon --atlas /data/atlas/earth.sqlite --latitude ${1%,} --longitude $2 --radius ${radius}
+  radius=${3:-100}
+  $AV/bazel-bin/atlas/tools/find_tiles_near_latlon --atlas /data/atlas/earth.sqlite --latitude ${1%,} --longitude $2 --radius ${radius}
 }
 
 visatlas(){
-    ~/codes/av/bazel-bin/atlas/tools/visualize_atlas --atlas $@
+  if [ ! -f $AV/bazel-bin/atlas/tools/visualize_atlas ]; then
+    pushd $AV
+    bazel build //atlas/tools:visualize_atlas
+    popd
+  fi
+  $AV/bazel-bin/atlas/tools/visualize_atlas --atlas $@
 }
 
 vistile() {
-    atlas="${2:-/data/atlas/earth.sqlite}"
-    ~/codes/av/bazel-bin/atlas/tools/visualize_atlas --atlas ${atlas} --root $1
+  atlas="${2:-/data/atlas/earth.sqlite}"
+  $AV/bazel-bin/atlas/tools/visualize_atlas --atlas ${atlas} --root $1
 }
 fosctl() {
-    ~/codes/av/bazel-bin/cloud/fan_out_service/fosctl/fosctl -- $@
+  $AV/bazel-bin/cloud/fan_out_service/fosctl/fosctl -- $@
+}
+
+cloudrun() {
+  gcloud compute --project "aurora-localization-dev" $1 --zone "us-east1-d" ${@[2,-1]}
 }
