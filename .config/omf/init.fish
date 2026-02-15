767a65c7e30a340d95b9394156dd114aaf2a9be5
Xiukun Hu <xiukun.hu@outlook.com>
2026-02-15 10:28:55 -0500

    remove fish

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -8,13 +8,8 @@ bind \ca beginning-of-line
 bind \ce end-of-line
 
 abbr erc nvim ~/.config/omf/init.fish
-abbr xa xrandr --auto
-abbr ofsc xrandr --output DP-3-1 --mode 1920x1080 --left-of DP-3-3 --output DP-3-3 --mode 1920x1080 --output eDP-1 --off
-abbr prn poetry run nvim
-
 abbr gco git checkout
 abbr gst git status
 abbr gcan! git commit -a --amend --no-edit
 abbr gb git branch -vv
 abbr killport --set-cursor "lsof -ti:% | xargs kill -9"
-# eval (direnv hook fish)
