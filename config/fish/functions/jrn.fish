9d8207b2b6e49ee304957456eb4239c06361f673
Xiukun Hu <xiukun.hu@outlook.com>
2026-03-06 18:29:45 -0500

    lazyvim plugins and jrn for journal

---
+++ config/fish/functions/jrn.fish
@@ -0,0 +1,3 @@
+function jrn --wraps=/Users/HuXiukun/Projects/life-system/scripts/journal.sh --wraps=/Users/HuXiukun/.scripts/journal.sh --description 'alias jrn /Users/HuXiukun/.scripts/journal.sh'
+    /Users/HuXiukun/.scripts/journal.sh $argv
+end
